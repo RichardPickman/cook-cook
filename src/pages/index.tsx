16d2f6bf1e7f408ba76a93a7483c2517f390883d
RichardPickman <drezzerock@gmail.com>
2023-06-26 16:53:22 +0200

    index file fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,11 @@ import { Header } from '@/components/Header';
 import { Preview } from '@/components/Preview';
 import { ForWho } from '@/components/ForWho';
 import { Menu } from '@/components/Menu';
-import { Prices } from '@/components/Prices';
 import { Learn } from '@/components/Learn';
 
 export default function Home() {
     return (
-        <main className="flex flex-col gap-20 mx-auto">
+        <main className="mx-auto flex flex-col gap-20">
             <Header />
             <Preview />
             <ForWho />
