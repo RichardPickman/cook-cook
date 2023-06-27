1bde6a1076b9abc57f06a49a60cef15ec6c8dd7e
RichardPickman <drezzerock@gmail.com>
2023-06-27 14:13:19 +0200

    add muted property to video for autoPlay

--- src/components/Cooking/index.tsx
+++ src/components/Cooking/index.tsx
@@ -2,10 +2,11 @@ export const Cooking = () => {
     return (
         <div id="cooking">
             <video
-                autoPlay={true}
-                loop
                 className="h-screen w-screen object-cover brightness-50"
                 src="video/video.mp4"
+                autoPlay
+                muted
+                loop
             />
         </div>
     );
